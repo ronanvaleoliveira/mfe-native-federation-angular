e389d8f0e70dd7f87b4215f66bb7b7e6cbe48373
Ronan Vale de Oliveira <ronan.oliveira@totvs.com.br>
2024-05-14 17:38:23 -0300

    1-Ajuste no AppComponent indicando a configuração standalone. 2-Ajuste nas configurações do bootstrap. 3-Remoção da declaração do AppComponent no app.module.

--- mfe-1/src/bootstrap.ts
+++ mfe-1/src/bootstrap.ts
@@ -1,7 +1,19 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './environments/environment';
+//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
 
+if (environment.production) {
+  enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+  ]
+});
