4e8ac8673eec0a0edbe658cd7dfacfe535075c52
Ronan Vale de Oliveira <ronan.oliveira@totvs.com.br>
2024-05-16 18:36:44 -0300

    Ajuste para trabalhar com environment e auxiliar na tomada de decisão ao carregar o arquivo  federation.manifest.json

--- mfe-host/src/main.ts
+++ mfe-host/src/main.ts
@@ -1,6 +1,12 @@
 import { initFederation } from '@angular-architects/native-federation';
+import { environment } from './environments/environment';
 
-initFederation('./assets/federation.manifest.json')
+
+const federationManifest = environment.production
+  ? './assets/federation.manifest.prod.json'
+  : './assets/federation.manifest.json';
+
+initFederation(federationManifest)
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
